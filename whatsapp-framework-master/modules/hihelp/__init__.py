ed3d7d39b1eefe0be140207cb8c0c075971b34bb
root <root@srv-ubuntu.home>
2018-12-20 06:10:42 -0200

    Alteracao nos horarios

--- whatsapp-framework-master/modules/hihelp/__init__.py
+++ whatsapp-framework-master/modules/hihelp/__init__.py
@@ -7,9 +7,9 @@ def __init__(message):
 		now = datetime.now()
 		if now.hour == 0:
 			mac.send_message("Bom dia! Obrigado por avisar", message.conversation)
-		if ((now.hour > 0) and (now.hour < 12)):
+		if ((now.hour > 0) and (now.hour < 18)):
 			mac.send_message("Boa tarde! Obrigado por avisar", message.conversation)
-		if ((now.hour > 12) and (now.hour < 24)):
+		if ((now.hour > 18) and (now.hour < 24)):
 			mac.send_message("Boa noite! Obrigado por avisar", message.conversation)
 	if message.text != "Não vou hoje":
 		mac.send_message("Por favor, só responda se irá hoje ou não. Obrigado", message.conversation)
