030ec1ad910b69caaa6dd7a833e83ad11cd8b488
root <root@srv-ubuntu.home>
2018-12-20 22:39:14 -0200

    Reconhecimento de quem esta enviando a msg

--- whatsapp-framework-master/modules/hihelp/__init__.py
+++ whatsapp-framework-master/modules/hihelp/__init__.py
@@ -1,15 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from yowsup.layers.protocol_messages.protocolentities import *
+import string
+from pprint import pprint
 from app.mac import mac, signals
 from datetime import datetime
+
 
 @signals.message_received.connect
 def __init__(message):
 	if message.text == "Não vou hoje":
 		now = datetime.now()
 		if ((now.hour >= 0) and (now.hour <=12)):
-			mac.send_message("Bom dia! Obrigado por avisar", message.conversation)
+				mac.send_message("Bom dia " + message.who_name + "! Obrigado por avisar", message.conversation)
 		if ((now.hour > 12) and (now.hour <= 18)):
-			mac.send_message("Boa tarde! Obrigado por avisar", message.conversation)
+				mac.send_message("Boa tarde "+ message.who_name + "! Obrigado por avisar", message.conversation)
 		if ((now.hour > 18) and (now.hour <= 24)):
-			mac.send_message("Boa noite! Obrigado por avisar", message.conversation)
+				mac.send_message("Boa noite "+ message.who_name + "! Obrigado por avisar", message.conversation)
 	if message.text != "Não vou hoje":
-		mac.send_message("Por favor, só responda se irá hoje ou não. Obrigado", message.conversation)
+			mac.send_message("Por favor " + message.who_name + ", só responda se irá hoje ou não. Obrigado", message.conversation)
